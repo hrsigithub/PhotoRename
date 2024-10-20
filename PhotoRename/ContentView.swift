464c5cce8d521e6a968bfdfa4340a584cb9310ed
Hiroshi Nakai <hrsi.ad@gmail.com>
2024-10-20 09:35:24 +0900

    Initial Commit

---
+++ PhotoRename/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoRename
+//
+//  Created by Hiroshi.Nakai on 2024/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoRename/PhotoRenameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRenameApp.swift
+//  PhotoRename
+//
+//  Created by Hiroshi.Nakai on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoRenameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
